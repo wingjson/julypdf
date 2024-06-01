2af54dfe66f71359ed9aec8ae3ba026265bf8342
wingjson <wingjson@outlook.com>
2024-06-01 14:57:27 +0800

    build command

--- qpdf/build_qpdf.go
+++ qpdf/build_qpdf.go
@@ -5,6 +5,6 @@ package qpdf
 /*
 #cgo CFLAGS: -I../include
 #cgo linux,amd64,!musl LDFLAGS: -lgcc_s -lstdc++ -ljpeg -lz  -lssl -lcrypto -lgnutls -lqpdf
-#cgo windows,amd64 LDFLAGS: -lgcc_s -lstdc++ -lmingwex -ljpeg -lz -lgnutls -lssl -lcrypto -lws2_32 -lqpdf
+#cgo windows,amd64 LDFLAGS: -lgcc_s -lstdc++ -lqpdf  -lmingwex -ljpeg -lz -lgnutls -lssl -lcrypto -lws2_32 -LD:/Code/mysy2/ucrt64/lib
 */
 import "C"
